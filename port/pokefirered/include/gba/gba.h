17eeb3fea23cc9b284f987cb653822ccd89b9088
Julien QUENTIN <jqntn88@gmail.com>
2026-03-19 23:13:55 +0100

    Reorder gba.h inclusions to fix ALIGNED macro undefined error on Linux

--- port/pokefirered/include/gba/gba.h
+++ port/pokefirered/include/gba/gba.h
@@ -1,10 +1,10 @@
 #ifndef GUARD_GBA_GBA_H
 #define GUARD_GBA_GBA_H
 
+#include "gba/defines.h"
 #include "gba/types.h"
 #include <string.h>
 
-#include "gba/defines.h"
 #include "gba/io_reg.h"
 #include "gba/isagbprint.h"
 #include "gba/macro.h"
