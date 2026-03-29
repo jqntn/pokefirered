c1624a091dfcfbbe4d6586dc8c49b5df4db02d37
Julien QUENTIN <jqntn88@gmail.com>
2026-03-29 21:15:55 +0200

    revert include/gba/gba.h

--- include/gba/gba.h
+++ include/gba/gba.h
@@ -1,11 +1,10 @@
 #ifndef GUARD_GBA_GBA_H
 #define GUARD_GBA_GBA_H
 
-#include "defines.h"
-#include "types.h"
-#include "io_reg.h"
 #include <string.h>
-
+#include "defines.h"
+#include "io_reg.h"
+#include "types.h"
 #include "multiboot.h"
 #include "syscall.h"
 #include "macro.h"
