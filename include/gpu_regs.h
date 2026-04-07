ff9f419f27a3a2523945ee78c0962519edcb5f57
Julien QUENTIN <jqntn88@gmail.com>
2026-04-07 14:50:32 +0200

    restore include/gpu_regs.h

--- include/gpu_regs.h
+++ include/gpu_regs.h
@@ -11,6 +11,7 @@
 void InitGpuRegManager(void);
 void CopyBufferedValuesToGpuRegs(void);
 void SetGpuReg(u8 regOffset, u16 value);
+void SetGpuReg_ForcedBlank(u8 regOffset, u16 value);
 u16 GetGpuReg(u8 regOffset);
 void SetGpuRegBits(u8 regOffset, u16 mask);
 void ClearGpuRegBits(u8 regOffset, u16 mask);
